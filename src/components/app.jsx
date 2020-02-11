a47dfe09a01c4f0685962ca0f31369f90a82b8f3
Merel <merel.steenbrink@gmail.com>
2020-02-11 15:38:41 +0100

    add App component and basic css

---
+++ src/components/app.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+
+class App extends Component {
+	render(){
+		return (
+			<div>
+				<div className="flat-list">
+					<p> this is the list of flats</p>
+				</div>
+				<div className="map-container">
+					<p> this is the map </p>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+export default App
